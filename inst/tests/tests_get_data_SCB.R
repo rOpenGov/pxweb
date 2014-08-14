fa9a42064cb406189e758120135e97768dc58ab6
Måns Magnusson <mons.magnusson@gmail.com>
2014-08-14 13:52:01 +0200

    Moved examples to test of get_pxweb_data()

--- inst/tests/tests_get_data_SCB.R
+++ inst/tests/tests_get_data_SCB.R
@@ -73,40 +73,6 @@ test_that(desc="get_pxweb",{
   
 })
 
-cat("\nExample tests : ")
-
-test_that(desc="Documentation examples",{
-  # Test example in get_pxweb()
-
-  expect_that({ 
-    url <- paste(c(base_url("sweSCB", version = "v1", lang = "sv"),"AM","AM0102","AM0102A","KLStabell14LpMan"), collapse="/")
-  }, not(throws_error()))
-
-  expect_that({   
-    metadata <- get_pxweb_metadata(url)
-  }, not(throws_error()))
-  
-  expect_that({ 
-    sink(file=tempfile())
-    dims <- get_pxweb_dims(metadata)
-    sink()
-  }, not(throws_error()))
-
-  
-  expect_that({   
-    test <- get_pxweb(metadata$URL, dims=list(
-      Myndighet = "C02",
-      Kon = "*",
-      Heltiddeltid = "*",
-      ContentsCode = "*",
-      Tid = "*"))
-  }, not(throws_error()))
-})  
-
-
-
-cat("\nDownload tests: ")
-
 test_that(desc="Examples",{
   ptm <- proc.time()
   expect_that({
