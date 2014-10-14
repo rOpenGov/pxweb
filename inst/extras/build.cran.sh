c4b8f511d95646f64851a5e50406794c9b1b023e
antagomir <leo.lahti@iki.fi>
2014-10-14 11:28:31 +0200

    version number update

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -1,5 +1,5 @@
 /usr/bin/R CMD BATCH document.R
 /usr/bin/R CMD build ../../
-/usr/bin/R CMD check --as-cran pxweb_0.4.2.tar.gz
-/usr/bin/R CMD INSTALL pxweb_0.4.2.tar.gz
+/usr/bin/R CMD check --as-cran pxweb_0.4.21.tar.gz
+/usr/bin/R CMD INSTALL pxweb_0.4.21.tar.gz
 
