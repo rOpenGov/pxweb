1ec924a0614cab1f3786877dee7453a44c570f65
Leo Lahti <leo.lahti@iki.fi>
2016-10-27 16:56:13 +0200

    ok

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -1,6 +1,6 @@
-/usr/local/bin/R CMD BATCH document.R
-#/usr/local/bin/R CMD build ../../ --no-build-vignettes
-/usr/local/bin/R CMD build ../../ 
-/usr/local/bin/R CMD check --as-cran pxweb_0.5.53.tar.gz
-/usr/local/bin/R CMD INSTALL pxweb_0.5.53.tar.gz
+/usr/bin/R CMD BATCH document.R
+#/usr/bin/R CMD build ../../ --no-build-vignettes
+/usr/bin/R CMD build ../../ 
+/usr/bin/R CMD check --as-cran pxweb_0.6.3.tar.gz
+/usr/bin/R CMD INSTALL pxweb_0.6.3.tar.gz
 
