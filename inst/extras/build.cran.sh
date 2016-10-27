f655ca717daba4c869b570c150e280d4afa55ff1
Leo Lahti <leo.lahti@iki.fi>
2016-10-27 17:17:41 +0200

    Version number update to separate this from CRAN release version

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -1,6 +1,6 @@
 /usr/bin/R CMD BATCH document.R
 #/usr/bin/R CMD build ../../ --no-build-vignettes
 /usr/bin/R CMD build ../../ 
-/usr/bin/R CMD check --as-cran pxweb_0.6.3.tar.gz
-/usr/bin/R CMD INSTALL pxweb_0.6.3.tar.gz
+/usr/bin/R CMD check --as-cran pxweb_0.6.32.tar.gz
+/usr/bin/R CMD INSTALL pxweb_0.6.32.tar.gz
 
