4f77829e2010fcfee9c324a22388fd86b49715eb
antagomir <leo.lahti@iki.fi>
2016-12-12 09:07:44 +0100

    fixing conflict

--- inst/extras/build.cran.sh
+++
@@ -1,6 +0,0 @@
-/usr/local/bin/R CMD BATCH document.R
-#/usr/local/bin/R CMD build ../../ --no-build-vignettes
-/usr/local/bin/R CMD build ../../ 
-/usr/local/bin/R CMD check --as-cran pxweb_0.5.53.tar.gz
-/usr/local/bin/R CMD INSTALL pxweb_0.5.53.tar.gz
-
