a2537491b7dd17da744ba74b3dfb85a1ced26f74
antagomir <leo.lahti@iki.fi>
2018-10-19 12:38:03 +0300

    Closes #144

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -2,6 +2,6 @@
 #/home/lei/bin/R CMD BATCH document.R
 #/home/lei/bin/R CMD build ../../ --no-build-vignettes
 /home/lei/bin/R-3.5.1/bin/R CMD build ../../ 
-/home/lei/bin/R-3.5.1/bin/R CMD check --as-cran pxweb_0.6.37.tar.gz
-/home/lei/bin/R-3.5.1/bin/R CMD INSTALL pxweb_0.6.37.tar.gz
+/home/lei/bin/R-3.5.1/bin/R CMD check --as-cran pxweb_0.6.38.tar.gz
+/home/lei/bin/R-3.5.1/bin/R CMD INSTALL pxweb_0.6.38.tar.gz
 
