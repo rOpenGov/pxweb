cb200e55132098aa83b526b2b37628f6b39d9f9c
Måns Magnusson <mons.magnusson@gmail.com>
2018-10-25 23:26:52 +0300

    Updated docs

--- R/pxweb_load_save.R
+++ R/pxweb_load_save.R
@@ -1,8 +1,14 @@
+#' Save and load \code{pxweb} objects from R temp folder
+#'  
+#' @param obj a \code{pxweb} object
+#' @keywords internal
 save_pxweb <- function(obj){
   checkmate::assert_class(obj, "pxweb")
   save(obj, file = obj$paths$rda_file_path, compress = FALSE)
 }
 
+#' @keywords internal
+#' @rdname save_pxweb
 load_pxweb <- function(obj){
   checkmate::assert_class(obj, "pxweb")
   load_path <- obj$paths$rda_file_path
@@ -11,12 +17,23 @@ load_pxweb <- function(obj){
   obj
 }
 
+#' @keywords internal
+#' @rdname save_pxweb
 load_pxweb_calls <- function(obj){
   checkmate::assert_class(obj, "pxweb")
   load_pxweb(obj)$calls
 }
 
+#' @keywords internal
+#' @rdname save_pxweb
 load_pxweb_config <- function(obj){
   checkmate::assert_class(obj, "pxweb")
   load_pxweb(obj)$config
+}
+
+#' @keywords internal
+#' @rdname save_pxweb
+load_pxweb_api_subpath <- function(obj){
+  checkmate::assert_class(obj, "pxweb")
+  load_pxweb(obj)$paths$api_subpath
 }
