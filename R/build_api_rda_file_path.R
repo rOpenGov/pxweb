44b185ebdf860e3790793e706912822061317232
Måns Magnusson <mons.magnusson@gmail.com>
2018-10-25 23:26:06 +0300

    Remove files

---
+++ R/build_api_rda_file_path.R
@@ -0,0 +1,45 @@
+#' Build or get the tmp_rda_file_path from an url or pxweb_api_s3 object
+#' 
+#' @details 
+#' The hostname is used to
+#'
+#' @param x object to create tmp_file_path for.
+#' 
+#' @keywords internal
+build_pxweb_rda_file_path <- function(x){
+  UseMethod("build_pxweb_rda_file_path")
+}
+
+#' @rdname build_pxweb_rda_file_path
+#' @keywords internal
+build_pxweb_rda_file_path.character <- function(x){
+  checkmate::assert_string(x)
+  parsed_url <- parse_url_or_fail(x)
+  build_pxweb_rda_file_path(parsed_url)  
+}
+
+#' @rdname build_pxweb_rda_file_path
+#' @keywords internal
+build_pxweb_rda_file_path.url <- function(x){
+  checkmate::assert_class(x, "url")
+  tmp_dir <- file.path(tempdir(), "pxweb", "apis")
+  if(!dir.exists(tmp_dir)){
+    dir.create(tmp_dir, recursive = TRUE)
+  }
+  file.path(tmp_dir, paste0(make.names(x$hostname),".rda"))
+}
+
+#' @rdname build_pxweb_rda_file_path
+#' @keywords internal
+build_pxweb_rda_file_path.pxweb <- function(x){
+  checkmate::assert_class(x, "pxweb")
+  x$paths$rda_file_path
+}
+
+#' @rdname build_pxweb_rda_file_path
+#' @keywords internal
+build_pxweb_rda_file_path.list <- function(x){
+  assert_pxweb_url(x)
+  build_pxweb_rda_file_path(x$url)
+}
+
