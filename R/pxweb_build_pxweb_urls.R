a3124f4c1467c444755800cd4701a50644699701
Måns Magnusson <mons.magnusson@gmail.com>
2018-10-27 09:12:13 +0300

    Small fixes

---
+++ R/pxweb_build_pxweb_urls.R
@@ -0,0 +1,63 @@
+#' Build the url to a PXWEB api
+#' 
+#' @keywords internal
+#' 
+build_pxweb_url <- function(x){
+  UseMethod("build_pxweb_url")
+}
+
+#' @rdname build_pxweb_url
+#' @keywords internal
+build_pxweb_url.list <- function(x) {
+  assert_pxweb_url(x)
+  build_pxweb_url(x$url)
+}
+
+#' @rdname build_pxweb_url
+#' @keywords internal
+build_pxweb_url.pxweb <- function(x) {
+  build_pxweb_url(x$url)
+}
+
+#' @rdname build_pxweb_config_url
+#' @keywords internal
+build_pxweb_url.url <- function(x) {
+  scheme <- x$scheme
+  hostname <- x$hostname
+  if (!is.null(x$port)) {
+    port <- paste0(":", x$port)
+  } else {
+    port <- NULL
+  }
+  path <- paste(gsub("^/", "", x$path), collapse = "/")
+  paste0(scheme, "://", hostname, port, "/", path)
+}
+
+
+#' Build the url to get the config from a PXWEB api
+#' 
+#' @keywords internal
+#' 
+build_pxweb_config_url <- function(x){
+  UseMethod("build_pxweb_config_url")
+}
+
+#' @rdname build_pxweb_config_url
+#' @keywords internal
+build_pxweb_config_url.list <- function(x) {
+  assert_pxweb_url(x)
+  build_pxweb_config_url(x$url)
+}
+
+#' @rdname build_pxweb_config_url
+#' @keywords internal
+build_pxweb_config_url.pxweb <- function(x) {
+  build_pxweb_config_url(x$url)
+}
+
+#' @rdname build_pxweb_config_url
+#' @keywords internal
+build_pxweb_config_url.url <- function(x) {
+  paste0(build_pxweb_url(x), "?config")
+}
+
