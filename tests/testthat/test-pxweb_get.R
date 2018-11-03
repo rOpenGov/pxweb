3e808cf1b25414edd893041ba6b15a0c7c0c0968
Måns Magnusson <mons.magnusson@gmail.com>
2018-11-03 23:36:57 +0200

    Added tests

--- tests/testthat/test-pxweb_get.R
+++ tests/testthat/test-pxweb_get.R
@@ -21,6 +21,22 @@ test_that(desc="Constructor works as it should with Statistics Sweden",{
   expect_silent(px_data <- suppressWarnings(pxweb_get(url = url, query = json_query)))
   expect_output(print(px_data), regexp = "PXWEB DATA")
   
+  url <- "http://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0101/BE0101A/BefolkningNy"
+  json_query <- file.path(system.file(package = "pxweb"), "extdata", "test_files", "json_queries", "json_single_query_test.json")
+  expect_silent(px_data <- suppressWarnings(pxweb_get(url = url, query = json_query)))
+  expect_output(print(px_data), regexp = "PXWEB DATA")
+  
+  url <- "http://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0101/BE0101A/BefolkningNy"
+  json_query <- file.path(system.file(package = "pxweb"), "extdata", "test_files", "json_queries", "json_full_test_query.json")
+  expect_silent(px_data <- suppressWarnings(pxweb_get(url = url, query = json_query)))
+  expect_output(print(px_data), regexp = "PXWEB DATA")
+  
+  url <- "http://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0101/BE0101A/BefolkningNy"
+  json_query <- file.path(system.file(package = "pxweb"), "extdata", "examples", "json_big_query_example.json")
+  expect_silent(px_data <- suppressWarnings(pxweb_get(url = url, query = json_query, verbose = FALSE)))
+  expect_output(print(px_data), regexp = "PXWEB DATA")
+  expect_output(print(px_data), regexp = "255200 observations")
+
 })  
 
 
