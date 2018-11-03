5214c15204dcfc41741f0f30caa99c8dd54068da
Måns Magnusson <mons.magnusson@gmail.com>
2018-11-03 23:34:34 +0200

    Added new code

---
+++ R/pxweb_c.R
@@ -0,0 +1,59 @@
+#' Combine pxweb objects
+#'
+#' @param x a list with \code{pxweb} objects.
+#' 
+#' @keywords internal
+pxweb_c <- function(x){
+  checkmate::assert_class(x, "list")
+  if(length(x) == 1) return(x[[1]])
+  
+  if(inherits(x[[1]], "pxweb_data")) return(pxweb_data_c(x))
+  
+  stop("pxweb_c() not implemented for class '", class(x[[1]])[1], "'.", call. = FALSE)
+}
+
+
+#' Combine pxweb objects
+#'
+#' @param x a list with \code{pxweb} objects.
+#' 
+#' @keywords internal
+pxweb_data_c <- function(x){
+  for (i in seq_along(x)) {
+    checkmate::assert_class(x[[i]], "pxweb_data")
+  }
+  full_pxd <- x[[1]]
+
+  # Assert equal columns
+  for(i in 2:length(x)){
+    for(j in seq_along(full_pxd$columns)){
+      checkmate::assert_set_equal(full_pxd$columns[[j]]$code, x[[i]]$columns[[j]]$code)
+      checkmate::assert_set_equal(full_pxd$columns[[j]]$text, x[[i]]$columns[[j]]$text)
+      checkmate::assert_set_equal(full_pxd$columns[[j]]$type, x[[i]]$columns[[j]]$type)      
+    }
+  }
+  
+  # Add comments if not duplicated
+  json_comments <- character(length(full_pxd$comments)) 
+  for(i in seq_along(full_pxd$comments)){
+    json_comments[i] <- jsonlite::toJSON(full_pxd$comments[[i]])
+  }
+  for(i in 2:length(x)){
+    for(j in seq_along(x[[i]]$comments)){
+      json_new_comment <- jsonlite::toJSON(x[[i]]$comments[[j]])
+      if(!(json_new_comment %in% json_comments)) json_comments <- c(json_comments, json_new_comment)
+    }
+  }
+  full_pxd$comments <- list()
+  for(i in seq_along(json_comments)){
+    full_pxd$comments[[i]] <- jsonlite::fromJSON(json_comments[i])
+  }
+  
+  # Combine data
+  for(i in 2:length(x)){
+    full_pxd$data <- c(full_pxd$data, x[[i]]$data)
+  }
+  
+  checkmate::assert_class(full_pxd, "pxweb_data")
+  full_pxd
+}
