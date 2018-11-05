39fcd87894287322514c6eefba8104d44c2be703
Måns Magnusson <mons.magnusson@gmail.com>
2018-11-05 23:08:18 +0200

    fixed stuff

--- R/pxweb_as_data_frame.R
+++ R/pxweb_as_data_frame.R
@@ -1,8 +1,8 @@
 #' Convert object to a \code{data.frame}
 #' 
 #' @param x an object to convert to \code{data.frame}.
-#' @param row.names See \link{\code{as.data.frame}}.
-#' @param optional See \link{\code{as.data.frame}}.
+#' @param row.names See \code{\link{as.data.frame}}.
+#' @param optional See \code{\link{as.data.frame}}.
 #' @param ... See \link{\code{as.data.frame}}.
 #' @param stringsAsFactors See \link{\code{as.data.frame}}.
 #' @param column.name.source character: should \code{code} or \code{text} be used as column names?
@@ -53,40 +53,38 @@ pxweb_as_data_frame.pxweb_data <-  function(x, row.names = NULL, optional = FALS
 
 #' @rdname pxweb_as_data_frame
 #' @keywords internal
-pxweb_as_data_frame.pxweb_data_comment <-  function(x, row.names = NULL, optional = FALSE, ..., stringsAsFactors = FALSE){
-
-  checkmate::assert_character(row.names, len = pxdims[1], null.ok = TRUE)
-  checkmate::assert_choice(column.name.source, c("code", "text"))
+pxweb_as_data_frame.pxweb_data_comments <-  function(x, optional = FALSE, ..., stringsAsFactors = default.stringsAsFactors()){
   checkmate::assert_flag(optional)
   checkmate::assert_flag(stringsAsFactors)
   
-  # Fill out
-  df <- matrix("", ncol = pxdims[2], nrow = pxdims[1])
-  slot_idx <- c(rep(1, length(x$data[[1]]$key)), rep(2, length(x$data[[1]]$values)))
-  slot_pos <- c(1:length(x$data[[1]]$key), 1:length(x$data[[1]]$values))
-  for(i in 1:pxdims[1]){
-    for(j in 1:pxdims[2]){
-      df[i, j] <- x$data[[i]][[slot_idx[j]]][[slot_pos[j]]]
-    }
+  dfs <- list()
+  for(i in seq_along(x$comments)){
+    dfs[[i]] <- pxweb_as_data_frame(x$comments[[i]], optional = optional, ..., stringsAsFactors = FALSE)
   }
-  df <- as.data.frame(df, stringsAsFactors = FALSE, optional = optional, ...)
-  colnames(df) <- pxweb_data_colnames(x, column.name.source)
-  for(j in 1:pxdims[2]){
-    if(slot_idx[j] == 1){
-      if(stringsAsFactors){
-        df[, j] <- as.factor(df[, j])
-      }
-    } else {
-      df[, j] <- as.numeric(df[, j])
-    }
-  }
-  if(!is.null(row.names)) {
-    rownames(df) <- row.names
+  df <- do.call(rbind, dfs)
+  if(stringsAsFactors){
+    df$comment_type <- as.factor(df$comment_type)
+    df$comment <- as.factor(df$comment)    
   }
   df
 }
 
-
+#' @rdname pxweb_as_data_frame
+#' @keywords internal
+pxweb_as_data_frame.pxweb_data_comment <- function(x, optional = FALSE, ..., stringsAsFactors = default.stringsAsFactors()){
+  checkmate::assert_flag(optional)
+  checkmate::assert_flag(stringsAsFactors)
+  
+  df <- x$idx_data_frame
+  if(stringsAsFactors){
+    df$comment_type <- as.factor(class(x)[1])
+    df$comment <- as.factor(x$comment)
+  } else {
+    df$comment_type <- class(x)[1]
+    df$comment <- x$comment
+  }
+  df
+}
 
 
 
