15e785e7f3c29936b95aa947abaea132e430f2db
Måns Magnusson <mons.magnusson@gmail.com>
2018-11-05 22:13:43 +0200

    Added new comment structure

--- R/pxweb_data_comments.R
+++ R/pxweb_data_comments.R
@@ -19,25 +19,24 @@ pxweb_data_comments.pxweb_data <- function(x){
   checkmate::assert_class(x, "pxweb_data")
   obj <- list()
   
-  ctxt <- pxweb_data_colnames(x, type = "text")
-  ccode <- pxweb_data_colnames(x, type = "code")
-  
   for(i in seq_along(x$columns)){
     if(!is.null(x$columns[[i]]$comment)){
-      obj[[length(obj) + 1]] <- pxweb_data_column_comment(x$columns[[i]])
+      obj[[length(obj) + 1]] <- pxweb_data_column_comment(x, i)
     }
   }
   
   for(i in seq_along(x$comments)){
-    obj[[length(obj) + 1]] <- pxweb_data_value_comment(x$comments[[i]], ccode, ctxt)
+    obj[[length(obj) + 1]] <- pxweb_data_value_comment(x, i)
   }
   
   has_comment <- unlist(lapply(x$data, function(x) !is.null(x$comment)))
   comment_idx <- which(has_comment)
   for(i in seq_along(comment_idx)){
-    obj[[length(obj) + 1]] <- pxweb_data_obs_comment(x$data[[comment_idx[i]]], ccode, ctxt)
+    obj[[length(obj) + 1]] <- pxweb_data_obs_comment(x, comment_idx[i])
   }
 
+  obj$pxweb_data_dims <- pxweb_data_dim(x)
+  
   class(obj) <- c("pxweb_data_comments", "list")
   assert_pxweb_data_comments(obj)
   obj
@@ -45,43 +44,61 @@ pxweb_data_comments.pxweb_data <- function(x){
 
 
 #' Construct a \code{pxweb_data_comment} object
-#' @param x an object to convert to a \code{pxweb_data_comment} object
+#' @param x an \code{pxweb_data} to extract a \code{pxweb_data_comment} object from.
+#' @param column_idx the index of the column to extract.
+#' @param comment_idx the index of the comment to extract.
+#' @param obs_idx the index of the comment to extract.
 #' @keywords internal
-pxweb_data_column_comment <- function(x){
-  checkmate::assert_names(names(x), permutation.of = c("code", "text", "type", "comment"))
-  obj <- x[c("code", "text")]
+pxweb_data_column_comment <- function(x, column_idx){
+  checkmate::assert_class(x, "pxweb_data")
+  checkmate::assert_int(column_idx, lower = 1, upper = length(x$columns))
+  obj <- x$columns[[column_idx]][c("code", "text")]
   obj$value <- NULL 
-  obj$comment <- x$comment
+  obj$comment <- x$columns[[column_idx]]$comment
+  obj$idx_data_frame <- data.frame(row_no = NA, col_no = column_idx) 
   class(obj) <- c("column_comment", "pxweb_data_comment", "list")
   obj
 }
 
 #' @rdname pxweb_data_column_comment
 #' @keywords internal
-pxweb_data_value_comment <- function(x, column_code, column_text){
-  checkmate::assert_names(names(x), permutation.of = c("variable", "value", "comment"))
-  checkmate::assert_character(column_code)
-  checkmate::assert_subset(x$variable, choices = column_code)
-  checkmate::assert_character(column_text)  
-  obj <- list(code = x$variable, 
-              text = column_text[which(x$variable %in% column_code)], 
-              value = x$value,
-              comment = x$comment)
+pxweb_data_value_comment <- function(x, comment_idx){
+  checkmate::assert_class(x, "pxweb_data")
+  checkmate::assert_int(comment_idx, lower = 1, upper = length(x$comments))
+
+  column_text <- pxweb:::pxweb_data_colnames(x, type = "text")
+  column_code <- pxweb:::pxweb_data_colnames(x, type = "code")
+  comment_column_idx <- which(x$comments[[comment_idx]]$variable %in% column_code)
+  
+  obj <- list(code = x$comments[[comment_idx]]$variable, 
+              text = column_text[comment_column_idx], 
+              value = x$comments[[comment_idx]]$value,
+              comment = x$comments[[comment_idx]]$comment)
+  
+  slot_idx <- c(rep(1, length(x$data[[1]]$key)), rep(2, length(x$data[[1]]$values)))
+  slot_pos <- c(1:length(x$data[[1]]$key), 1:length(x$data[[1]]$values))
+  has_value <- unlist(lapply(x$data, 
+                             function(x, slot_idx, slot_pos, comment_column_idx, obj) x[[slot_idx[comment_column_idx]]][[slot_pos[comment_column_idx]]] == obj$value,
+                             slot_idx = slot_idx, slot_pos = slot_pos, comment_column_idx = comment_column_idx, obj = obj))
+  obj$idx_data_frame <- data.frame(row_no = which(has_value), col_no = rep(comment_idx, sum(has_value)))
   class(obj) <- c("value_comment", "pxweb_data_comment", "list")
   obj
 }
 
 #' @rdname pxweb_data_column_comment
 #' @keywords internal
-pxweb_data_obs_comment <- function(x, column_code, column_text){
-  checkmate::assert_names(names(x), permutation.of = c("key", "values", "comment"))
-  checkmate::assert_character(column_code)
-  checkmate::assert_character(column_text)  
+pxweb_data_obs_comment <- function(x, obs_idx){
+  checkmate::assert_class(x, "pxweb_data")
+  checkmate::assert_int(obs_idx, lower = 1, upper = length(x$data))
+
+  column_text <- pxweb:::pxweb_data_colnames(x, type = "text")
+  column_code <- pxweb:::pxweb_data_colnames(x, type = "code")
   
   obj <- list(code = column_code, 
               text = column_text, 
-              value = unname(unlist(x[c("key", "values")])),
-              comment = x$comment)
+              value = unname(unlist(x$data[[obs_idx]][c("key", "values")])),
+              comment = x$data[[obs_idx]]$comment)
+  obj$idx_data_frame <- data.frame(row_no = obs_idx, col_no = NA)
   class(obj) <- c("obs_comment", "pxweb_data_comment", "list")
   obj
 }
@@ -98,4 +115,5 @@ assert_pxweb_data_comments <- function(x){
                                                       "value_comment",
                                                       "column_comment"))
   }
+  checkmate::assert_integerish(x$pxweb_data_dims, lower = 1)
 }
