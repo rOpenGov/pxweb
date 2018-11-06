1a5fa21f8a27c07ce0965fde8585e61a50a52487
Måns Magnusson <mons.magnusson@gmail.com>
2018-11-06 21:20:58 +0200

    fixes

--- tests/testthat/test-pxweb_test_api_endpoint.R
+++ tests/testthat/test-pxweb_test_api_endpoint.R
@@ -4,7 +4,7 @@ context("pxweb_test_api_endpoint")
 
 test_that(desc="Mixed node levels object",{
   url <- "http://api.scb.se/OV0104/v1/doris/sv/ssd/START/AM/AM0301/"
-  expect_output(res <- suppressWarnings(pxweb_test_api_endpoint(url), regexp = "4 node.+and 14 table"))
+  expect_output(res <- suppressWarnings(pxweb_test_api_endpoint(url)), regexp = "4 node.+and 14 table")
   expect_true(all(res$checked))
   expect_true(all(!res$error))
 })  
