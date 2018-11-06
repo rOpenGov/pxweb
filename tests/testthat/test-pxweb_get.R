3b95cc752ffd012dd5b2d0de66b85585e46124c8
Måns Magnusson <mons.magnusson@gmail.com>
2018-11-06 08:49:50 +0200

    rewrite name

--- tests/testthat/test-pxweb_get.R
+++ tests/testthat/test-pxweb_get.R
@@ -51,7 +51,7 @@ test_that(desc="Constructor works as it should with Statistics Sweden",{
   expect_length(pxweb_data_comments(x = px_data), 2)
 })  
 
-test_that(desc="Mixed node meta data object",{
+test_that(desc="Mixed node levels object",{
   # This is a bug in the previous implementation of pxweb
   url <- "http://bank.stat.gl/api/v1/en/Greenland/BE/BE01"
   expect_silent(px_meta_data <- pxweb_get(url))
