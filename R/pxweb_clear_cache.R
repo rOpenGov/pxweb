bebdc4d62f333bdf9757c99c127316c51ee56338
Måns Magnusson <mons.magnusson@gmail.com>
2018-11-07 19:32:26 +0200

    small fixes

--- R/pxweb_clear_cache.R
+++ R/pxweb_clear_cache.R
@@ -9,6 +9,7 @@
 pxweb_clear_cache<- function(x = NULL){
   if(is.null(x)){
     files <- dir(file.path(tempdir(), "pxweb", "apis"), full.names = TRUE)
+    if(length(files) == 0) return(FALSE)
     sleep_s <- numeric(length(files))
     for(f in seq_along(files)){
       obj <- NULL # To capture a builde NOTE and safeguard for global variable handling
@@ -18,11 +19,13 @@ pxweb_clear_cache<- function(x = NULL){
     }
     Sys.sleep(max(sleep_s))
     file.remove(files)
+    return(TRUE)
   } else {
     checkmate::assert_class(x, "pxweb")
     time_diff_s <- as.numeric(Sys.time()) - as.numeric(x$calls$time_stamps[[1]])
     sleep_s <- max(x$config$period_in_seconds - time_diff_s, 0)
     Sys.sleep(sleep_s)
     if(file.exists(x$paths$rda_file_path)) file.remove(x$paths$rda_file_path)
+    return(TRUE)
   }
 }
