9791287f09b0883ac9c46049c663c6ae684bb148
Måns Magnusson <mons.magnusson@gmail.com>
2018-11-18 18:16:24 +0200

    Small addition tests

--- tests/testthat/test-pxweb_interactive.R
+++ tests/testthat/test-pxweb_interactive.R
@@ -8,6 +8,20 @@ test_that(desc="Basic usage",{
   expect_output(print(pxe), "api.scb.se")
   expect_output(print(pxe), "\\[\\[Region\\]\\]")  
   
+  expect_output(pxe_1 <- pxweb_interactive_input(pxe, test_input = "1"), "Separate multiple choices by")  
+  expect_output(print(pxe_1), "/OV0104/v1/doris/sv/ssd/START/ME/ME0104/ME0104C/ME0104T24")
+  expect_output(print(pxe_1), "api.scb.se")
+  expect_output(print(pxe_1), "\\[\\[Parti\\]\\]")  
+  
+  expect_output(pxe_b <- pxweb_interactive_input(pxe, test_input = "b"), "Separate multiple choices by")  
+  expect_output(print(pxe_b), "/OV0104/v1/doris/sv/ssd/START/ME/ME0104/ME0104C")
+  expect_output(print(pxe_b), "api.scb.se")
+  
+  # Incorrect input
+  expect_error(capture_output(pxe_error <- pxweb_interactive_input(pxe, test_input = "9999")), "incorrect")
+  
+  # expect_error(capture_output(pxe_error <- pxweb_interactive_input(pxe, test_input = "")), "incorrect")
+  
 })  
 
 
