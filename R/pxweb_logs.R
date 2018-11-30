894cc981d37b90deee8fa2f8780a018872fe7969
Måns Magnusson <mons.magnusson@gmail.com>
2018-11-30 18:46:34 +0100

    Finalized log handling

--- R/pxweb_logs.R
+++ R/pxweb_logs.R
@@ -10,15 +10,16 @@
 pxweb_http_log_on <- function(fn = "log_pxweb_api_http_calls.txt"){
   checkmate::assert_string(fn)
   log_file_path <- file.path(getwd(), fn)
+  internal_path <- pxweb_log_paths_path()
   checkmate::assert_path_for_output(log_file_path, overwrite = TRUE)
   if(file.exists(log_file_path)){
     file.remove(log_file_path)
   }
-  write(paste0("PXWEB API HTTP LOG ", Sys.time(), ":"), file = lp)
-  if(file.exists(pxweb_log_paths_path())){
-    file.remove(pxweb_log_paths_path())
+  write(paste0("PXWEB API HTTP LOG ", Sys.time(), ":"), file = log_file_path)
+  if(file.exists(internal_path)){
+    file.remove(internal_path)
   }
-  save(log_file_path, file = pxweb_log_paths_path())
+  save(log_file_path, file = internal_path)
 }
 
 #' @keywords internal
@@ -33,15 +34,40 @@ pxweb_http_log_off <- function(){
 #' @keywords internal
 #' @rdname pxweb_http_log_on
 pxweb_log_paths_path <- function(){
-  file.path(tempdir(), "pxweb", "log_http_calls.rda")
+  file.path(pxweb_tempdir(to = "logs"), "log_http_calls.rda")
+}
+
+
+#' @keywords internal
+#' @rdname pxweb_http_log_on
+pxweb_http_log_is_on <- function(){
+  lpp <- pxweb_log_paths_path()
+  file.exists(lpp)
 }
 
 #' @keywords internal
 #' @rdname pxweb_http_log_on
 pxweb_log_http_response <- function(r){
-  lp <- pxweb_log_paths_path()
-  if(!file.exists(lp)){
+  checkmate::assert_class(r, "response")
+  if(!pxweb_http_log_is_on()){
     return(invisible(NULL))
   }
+  log_file_path <- NULL # Load the file path next
+  load(pxweb_log_paths_path())
   
+  txt <- pxweb_response_to_log_as_json(r)
+  write(c(paste0("CALL AND RESPONSE FROM PXWEB API ", Sys.time(), ": "), txt), append = TRUE, file = log_file_path)
+  return(invisible(NULL))
+}
+
+#' @keywords internal
+#' @rdname pxweb_http_log_on
+pxweb_response_to_log_as_json <- function(r){
+  checkmate::assert_class(r, "response")
+  txt <- unclass(r)
+  txt$content <- "The content is removed to limit log file size."
+  txt$request <- unclass(txt$request)
+  txt$request$output <- unclass(txt$request$output)
+  txt$handle <- NULL
+  jsonlite::toJSON(txt, pretty = TRUE, auto_unbox = TRUE)
 }
