92f21d8c81b561c0b87d4bef444e5ff9a832ecd3
Måns Magnusson <mons.magnusson@gmail.com>
2018-12-01 07:28:21 -0500

    Fixed docs

--- R/pxweb-package.R
+++ R/pxweb-package.R
@@ -1,9 +1,15 @@
-#' @title pxweb
+#' @title Interface to PX-WEB APIs from R
 #' 
-#' @description Interface to the PX-WEB API
+#' @description 
+#' PXWEB is a common web API used by many European Statistical agencies to disseminate official statistics.
+#' The pxweb package facilitates connections and usage of these APIs.
+#' 
+#' @references 
+#' The decription of the PXWEB API here:
+#' \url{https://www.scb.se/en/About-us/about-the-website-and-terms-of-use/open-data-api/api-for-the-statistical-database/}
+#' The official home page of PXWEB can be found here:
+#' \url{https://www.scb.se/sv_/PC-Axis/Programs/PX-Web/}
 #' 
 #' @name pxweb-package
 #' @docType package
-#' @import data.table httr RJSONIO stringr methods
-#' @importFrom utils read.table maintainer
 NULL
