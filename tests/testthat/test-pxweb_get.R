0fc047436e2a0fbe6d24d49bed33f92bebf0e21b
Måns Magnusson <mons.magnusson@gmail.com>
2018-12-01 11:54:09 -0500

    Updated tests

--- tests/testthat/test-pxweb_get.R
+++ tests/testthat/test-pxweb_get.R
@@ -82,25 +82,6 @@ test_that(desc="Test to download json-stat objects",{
 })  
 
 
-test_that(desc="Converting pxweb data to matrices and data.frames",{
-  # Move to 
-  url <- "http://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0101/BE0101A/BefolkningNy"
-  json_query <- file.path(system.file(package = "pxweb"), "extdata", "examples", "json_query_example.json")
-  # save(px_data, file = "px_data_example.rda")
-  # load("px_data_example.rda")
-  expect_silent(px_data <- suppressWarnings(pxweb_get(url = url, query = json_query)))
-
-  expect_silent(df <- as.data.frame(px_data))
-  expect_s3_class(df, "data.frame")
-  expect_equal(dim(df), expected = c(3,3))
-  
-  expect_silent(mat <- as.matrix(px_data))
-  expect_true(is.matrix(mat))
-  expect_true(is.character(mat))
-  expect_equal(dim(mat), expected = c(3,3))
-})  
-
-
 test_that(desc="Test http logger",{
   pxweb_clear_cache()
   url <- "http://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0101/BE0101A/BefolkningNy"
