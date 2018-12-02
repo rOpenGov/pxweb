185e33a5751d28a8a70b146543c5b8f84881a8de
antagomir <leo.lahti@iki.fi>
2018-12-02 13:56:14 +0200

    Updated testthat_get_pxweb which now runs through without failure

--- tests/testthat_get_pxweb.R
+++ tests/testthat_get_pxweb.R
@@ -1,4 +1,5 @@
 library(testthat)
-Sys.sleep(pxweb::api_parameters()[["api.scb.se"]]$period_in_seconds)
+#Sys.sleep(pxweb::api_parameters()[["api.scb.se"]]$period_in_seconds)
+Sys.sleep(pxweb::pxweb_api_catalogue()[["api.scb.se"]]$period_in_seconds)
 test_check("pxweb", filter = "get_pxweb")
 
