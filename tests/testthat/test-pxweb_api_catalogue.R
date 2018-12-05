c171d84ad2a65814a5736138439f33ac86ce25f8
Måns Magnusson <mons.magnusson@gmail.com>
2018-12-05 09:40:16 -0500

    test again

--- tests/testthat/test-pxweb_api_catalogue.R
+++ tests/testthat/test-pxweb_api_catalogue.R
@@ -7,7 +7,7 @@ test_that(desc="pxweb_api_catalogue",{
   expect_silent(pxac <- pxweb_api_catalogue())
   expect_output(print(pxac), regexp = "Api:")
 
-  expect_silent(pxacgh <- pxweb_api_catalogue_from_github("test"))
+  expect_silent(pxacgh <- pxweb:::pxweb_api_catalogue_from_github("test"))
   expect_output(print(pxac), regexp = "Api:")
       
   expect_equal(pxac[[1]], pxacgh[[1]])
