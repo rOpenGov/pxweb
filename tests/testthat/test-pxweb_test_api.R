b489734be122df420d94759fc2e7e23e01a43eb4
Pyry Kantanen <pyry.kantanen@utu.fi>
2021-02-23 13:14:22 +0200

    Fix tests, assign global variable properly, fix copyright years

--- tests/testthat/test-pxweb_test_api.R
+++ tests/testthat/test-pxweb_test_api.R
@@ -20,7 +20,7 @@ test_that(desc="Mixed node levels object",{
   expect_silent(api_paths <- pxweb:::pxweb_test_create_api_paths(apis = pxweb_api_catalogue()))
   expect_true(all(c("http://api.scb.se/OV0104/v1/doris/en", 
                     "http://api.scb.se/OV0104/v1/doris/sv", 
-                    "http://pxnet2.stat.fi/PXWeb/api/v1/en",
-                    "http://pxnet2.stat.fi/PXWeb/api/v1/fi",
-                    "http://pxnet2.stat.fi/PXWeb/api/v1/sv") %in% api_paths$paths))
+                    "https://pxnet2.stat.fi/PXWeb/api/v1/en",
+                    "https://pxnet2.stat.fi/PXWeb/api/v1/fi",
+                    "https://pxnet2.stat.fi/PXWeb/api/v1/sv") %in% api_paths$paths))
 })  
