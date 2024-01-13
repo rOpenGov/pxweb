473d960cf9e3678a4686c91f586efb1bb506415d
Mans Magnusson <mons.magnusson@gmail.com>
2024-01-13 17:14:03 +0100

    Minor fix

--- R/pxweb.R
+++ R/pxweb.R
@@ -21,8 +21,10 @@
 #'
 #' @examples
 #' \dontrun{
-#' pxapi_1 <- pxweb(url = "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/ME/ME0104/ME0104C/ME0104T24")
-#' pxapi_2 <- pxweb(url = "https://api.scb.se/OV0104/v1/doris/sv")
+#' pxapi_1 <-
+#'   pxweb("https://api.scb.se/OV0104/v1/doris/sv/ssd/START/ME/ME0104/ME0104C/ME0104T24")
+#' pxapi_2 <-
+#'   pxweb(url = "https://api.scb.se/OV0104/v1/doris/sv")
 #' }
 #'
 #' @export
