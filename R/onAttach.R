8409be67f4d20dc84ea6a4c8b8ec79d823cc4dba
Pyry Kantanen <pyry.kantanen@gmail.com>
2025-03-31 20:32:07 +0300

    Update packageStartupMessage

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,3 +1,3 @@
 .onAttach <- function(lib, pkg) {
-  packageStartupMessage(paste0("pxweb ", utils::packageVersion("pxweb"), ": R tools for the PX-WEB API.\nhttps://github.com/ropengov/pxweb\n"))
+  packageStartupMessage(paste0("pxweb ", utils::packageVersion("pxweb"), ": R Interface to PXWEB APIs.\nhttps://github.com/ropengov/pxweb\n"))
 }
