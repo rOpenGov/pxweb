72cc8707ea9a70c5ae0516844197efa2420fa808
Måns Magnusson <mons.magnusson@gmail.com>
2026-03-02 07:40:11 +0100

    Add test that work

--- tests/testthat/test-pxweb_v2.R
+++ tests/testthat/test-pxweb_v2.R
@@ -5,16 +5,20 @@ test_that(desc = "pxweb API v2", {
   skip_if_offline()
 
   # API v1
-  pxapi_1 <- pxweb(url = "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/ME/ME0104/ME0104C/ME0104T24")
+  expect_silent(pxapi_1 <- pxweb(url = "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/ME/ME0104/ME0104C/ME0104T24"))
   expect_equal(pxapi_1$version, "v1")
 
-  pxapi_2 <- pxweb(url = "https://api.scb.se/OV0104/v1/doris/sv")
+  expect_silent(pxapi_2 <- pxweb(url = "https://api.scb.se/OV0104/v1/doris/sv"))
   expect_equal(pxapi_2$version, "v1")
 
   # Example table id from SCB v2 documentation.
   metadata_url <- "https://statistikdatabasen.scb.se/api/v2/tables/TAB5974/metadata"
-  pxapi_v2 <- pxweb(url = metadata_url)
+  expect_silent(pxapi_v2 <- pxweb(url = metadata_url))
   expect_equal(pxapi_v2$version, "v2")
+
+  # Metadata is working
+  metadata_url <- "https://statistikdatabasen.scb.se/api/v2/tables/TAB5974/metadata"
+  expect_silent(pxapi_v2 <- pxweb_get(url = metadata_url))
 
 })
 
