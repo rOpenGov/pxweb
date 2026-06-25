6f35d8739ef55edd4640fa893ffb3abcb241d650
Måns Magnusson <mons.magnusson@gmail.com>
2026-06-25 10:54:48 +0200

    Fixes

--- R/pxweb_table_response_v2.R
+++ R/pxweb_table_response_v2.R
@@ -6,7 +6,7 @@
 #' @param x a list returned from a PXWEB API to convert to a \code{table_response_v2} object.
 #'
 #' @return
-#' a \code{pxweb_table_v2} object.
+#' a \code{pxweb_table_response_v2} object.
 #'
 #' @keywords internal
 pxweb_table_response_v2 <- function(x) {
@@ -16,12 +16,10 @@ pxweb_table_response_v2 <- function(x) {
   x
 }
 
-#' Assert that x is a correct \code{pxweb_data_jsonstat} object.
-#' Assert a json-stat version 1.0 or later object
+#' Assert that x is a correct \code{pxweb_table_response_v2} object.
 #' @param x an object to check.
 #' @keywords internal
 assert_pxweb_table_response_v2 <- function(x) {
   checkmate::assert_class(x, c("list"))
   checkmate::assert_names(names(x), must.include = c("language", "id", "label", "updated", "firstPeriod", "lastPeriod", "variableNames", "links"))
 }
-
